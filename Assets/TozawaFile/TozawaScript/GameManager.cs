5d4134dc5bd435a12e1f3975daa2b9972ae77a97
st37071556@icloud.com <st37071556@icloud.com>
2022-06-06 13:58:48 +0900

    UI変更

--- Assets/TozawaFile/TozawaScript/GameManager.cs
+++ Assets/TozawaFile/TozawaScript/GameManager.cs
@@ -8,8 +8,10 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }//インスタンス
     [SerializeField] bool _hideMousecCursor = false;
     [SerializeField] Text _scoreText;
+    [SerializeField] float _restartWaitTime = 1f;
     [SerializeField] UnityEngine.Events.UnityEvent _onGameStart = null;//ゲーム開始時に呼び出す処理
-    [SerializeField] UnityEngine.Events.UnityEvent _onGameover = null;//ゲームクリア時に呼び出す処理
+    [SerializeField] UnityEngine.Events.UnityEvent _onGameover = null;//ゲームオーバー時に呼び出す処理
+    [SerializeField] UnityEngine.Events.UnityEvent _onGameClear = null;//ゲームオーバー時に呼び出す処理
     public HPBar hpbar;
     int _score = 0;
     private void Awake()
@@ -19,32 +21,44 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         CursorCheck();
-        
+        GameStart();
     }
 
-    void CursorCheck()
+    void CursorCheck()// マウスを消す
     {
         if (_hideMousecCursor)
-        { Cursor.visible = false; }// マウスを消す
+        { Cursor.visible = false; }
     }
-    public void GameStart()
+    public void GameStart()//ゲーム開始・リスタート時に呼ばれる関数
     {
         Trigger.trigger = false;
         _onGameStart.Invoke();
     }
-    public  void GameOver()
+    public  void GameOver()//失敗時呼ばれる 条件 花炎上
     {
-        _score = hpbar.currentHp;
-        StartCoroutine(WaitAnimation(_score));
+        StartCoroutine(WaitAnimation(0));
+        
+    }
+    public void GameClear()// 成功時呼ばれる 条件 花接触
+    {
+        _score = hpbar.currentHp;//どれだけ花をのばしたか
+        StartCoroutine(WaitAnimation(_score));//をスコアにする
     }
     
     public  IEnumerator WaitAnimation(int score)
     {
         Debug.Log("Waiting");
+        if (score == 0)
+        {
+            _onGameover.Invoke();
+        }
+        else if(score > 0)
+        {
+            _onGameClear.Invoke();
+            _scoreText.text = _score.ToString("D8");
+        }
         yield return new WaitUntil(() => Trigger.trigger);
         //ここにアニメーションが完了してからしてほしいことを書く
-        _onGameover.Invoke();
-        _scoreText.text = _score.ToString("D8");
-        
+        Invoke(nameof(GameStart), _restartWaitTime); //自動リスタート
     }
 }
