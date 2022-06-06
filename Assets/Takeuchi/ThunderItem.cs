f665a6bdcc0da6d7b3d6415ad968d1a74aeebd16
maya0117 <mynhjm@icloud.com>
2022-06-06 15:26:58 +0900

    prefabにmusicを追加しました

--- Assets/Takeuchi/ThunderItem.cs
+++ Assets/Takeuchi/ThunderItem.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ThunderItem : MonoBehaviour
 {
+    [SerializeField]
+    AudioClip audio = null;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            if (audio)
+            {
+                AudioSource.PlayClipAtPoint(audio, transform.position);
+            }
             CloudContact cloud = collision.GetComponent<CloudContact>();
             if (cloud != null)
             {
