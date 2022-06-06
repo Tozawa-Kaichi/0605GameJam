95ca63f6363eb291d0c6b14663b5d373384c2596
st37071556@icloud.com <st37071556@icloud.com>
2022-06-06 12:58:44 +0900

    GameManager クリアUI スコア表示等

--- Assets/TozawaFile/TozawaScript/GameManager.cs
+++ Assets/TozawaFile/TozawaScript/GameManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+
     public static GameManager Instance { get; private set; }//インスタンス
     [SerializeField] bool _hideMousecCursor = false;
+    [SerializeField] Text _scoreText;
     [SerializeField] UnityEngine.Events.UnityEvent _onGameStart = null;//ゲーム開始時に呼び出す処理
     [SerializeField] UnityEngine.Events.UnityEvent _onGameover = null;//ゲームクリア時に呼び出す処理
+    public HPBar hpbar;
     int _score = 0;
     private void Awake()
     {
@@ -27,8 +30,8 @@ public class GameManager : MonoBehaviour
     }
     public  void GameOver()
     {
-        //_score =HPBar;
-        _onGameover.Invoke();
+        _score = hpbar.currentHp;
+        StartCoroutine(WaitAnimation(_score));
 
     }
     public void Some(int score)
@@ -40,5 +43,8 @@ public class GameManager : MonoBehaviour
         Debug.Log("Waiting");
         yield return new WaitUntil(() => Trigger.trigger);
         //ここにアニメーションが完了してからしてほしいことを書く
+        _onGameover.Invoke();
+        _scoreText.text = _score.ToString("D8");
+        
     }
 }
